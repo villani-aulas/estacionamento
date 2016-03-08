96be17c924cdf73f8ab31bcf13f28d6535c512b3
Leonardo Villani <informatica@villani.eti.br>
2016-03-07 21:54:42 -0300

    Estruturação da página inicial do projeto.

--- estacionamento/index.php
+++ estacionamento/index.php
@@ -1,2 +1,10 @@
-<?php
-print "Página inicial do projeto!";
+<!doctype html>
+<html lang="pt-BR">
+<head>
+	<meta charset="UTF-8" />
+	<title>Página inicial do projeto</title>
+</head>
+<body>
+	<h1>Página inicial do projeto</h1>
+</body>
+</html>
